73b2083a4682fdc56d28560afc6373261e13e7ff
WangCheng0116 <e1088136@u.nus.edu>
2023-05-09 10:20:21 +0800

    Initial Commit

---
+++ itinerary/itineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  itineraryApp.swift
+//  itinerary
+//
+//  Created by 王小程 on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct itineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
